bef34fc0d20365f8f2160798099bf4b0530d29f9
JhonEduard26 <jhon.bocanegra26@gmail.com>
2023-08-24 23:38:31 +0000

    first design card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { jobs } from './data.json'
 import JobCard from './components/JobCard.vue'
 </script>
 
 <template>
-  <JobCard msg="Jhon"/>
+  <div v-for="job in jobs" :key="job.id">
+    <JobCard
+      :companyName="job.company"
+      :salary="job.salary"
+      :publishDate="job.date"
+      :name="job.name"
+      :positions="job.positions"
+      :tags="job.tags"
+    />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/TagPill.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+defineProps({
+  name: String
+})
+
+</script>
+
+<template>
+  <div>
+    <span>
+      {{ name }}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: inline-flex;
+    padding: 4px 8px;
+    border-radius: 6px;
+    border: 1px solid lightgray;
+    background-color: white;
+  }
+  div span {
+    font-size: 0.85rem
+  }
+</style>
